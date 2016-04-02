38dd11bc51f55193d30ca1fc9591f84d589beacb
ioiasff <arash.github@gmail.com>
2016-04-02 19:15:35 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,30 +226,30 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {152485254,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
-An advance Administration bot based on yagop/telegram-bot 
+    about_text = [[GPBOT v2 
+G P B O T
 
-https://github.com/SEEDTEAM/TeleSeed
+ANTI SPAM!
 
 Our team!
-Alphonse (@Iwals)
-I M /-\ N (@Imandaneshi)
-Siyanew (@Siyanew)
-Rondoozle (@Potus)
-Seyedan (@Seyedan25)
+A
+R
+A
+S
+H
 
-Special thanks to:
-Juan Potato
-Siyanew
-Topkecleon
-Vamptacus
+G
+P
+B
+O
+T
 
 Our channels:
-English: @TeleSeedCH
-Persian: @IranSeed
+English: @HACKCOLLECTION
+Persian: @HACKCOLLECTION
 ]],
     help_text_realm = [[
 Realm Commands:
